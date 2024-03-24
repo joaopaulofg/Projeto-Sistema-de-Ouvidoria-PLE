496fe781158087f24e7a94aa449f588413d1bce6
joaopaulofg <joao.gomes@ccc.ufcg.edu.br>
2024-03-24 13:24:53 -0300

    subindo as funcionalidades de listar e excluir manifestações

--- Ouvidoria/lib.py
+++ Ouvidoria/lib.py
@@ -61,13 +61,48 @@ def inserirManifestacao():
         try:
             c = con.cursor()
         except:
-            print("Erro ao conectar no banco de dados;")
+            print('\033[1;31mErro ao tentar conectar-se ao banco de dados.\033[m')
         else:
             try:
                 sql = ('INSERT INTO ouvidoria(cpfManifestante, manifestante, manifestacao, telefone_1) SELECT "'+cpfManifestante+'", "'+manifestante+'", "'+manifestacao+'", "'+telefone_1+'"')
                 c.execute(sql)
             except:
-                print("Erro ao inserir manifestação.")
+                print("\nErro ao inserir manifestação.")
             else:
-                print("Manifestação inserido com sucesso!")
+                print("\nManifestação inserido com sucesso!")
                 con.commit()
+
+# Listagem de manifestações existentes no sistema.
+def listarManifestacoes():
+    try:
+        c = con.cursor()
+    except:
+        print('\033[1;31mErro ao tentar conectar-se ao banco de dados.\033[m')
+    else:
+        try:
+            sql = 'SELECT * FROM ouvidoria'
+            c.execute(sql)
+        except:
+            print("Erro ao listar manifestações.")
+        else:
+            cabecalho("manifestações cadastradas")
+            manifestacoes = c.fetchall()
+            for m in manifestacoes:
+                print("\nID Manifestação:", m[0], "\nCPF Manifestante:", m[1], "\nManifestante:", m[2], "\nRelato:", m[3])
+
+# Exclusão de manifestações existentes no sistema.
+def excluirManifestacao():
+    idExclusao = lerInteiro("\nInsira o ID da manifestação que deseja excluir: ")
+    try:
+        c = con.cursor()
+    except:
+        print('\033[1;31mErro ao tentar conectar-se ao banco de dados.\033[m')
+    else:
+        try:
+            dsql = f'DELETE FROM ouvidoria WHERE id = {idExclusao}'
+            c.execute(dsql)
+        except:
+            print("Manifestação não encontrada.")
+        else:
+            print("A manifestação com ID ", idExclusao, " foi apagada com sucesso!")
+            con.commit()
--- Ouvidoria/ouvidoria.py
+++ Ouvidoria/ouvidoria.py
@@ -1,4 +1,4 @@
-from lib import cabecalho, menuPrincipal, lerInteiro,inserirManifestacao
+from lib import cabecalho, menuPrincipal, lerInteiro, inserirManifestacao, listarManifestacoes, excluirManifestacao
 import os
 
 cabecalho("ouvidoria do joão")
@@ -9,13 +9,13 @@ while True:
     op = lerInteiro("ESCOLHA UMA OPÇÃO: ")
 
     if op == 1:
-        print()
+        listarManifestacoes()
     elif op == 2:
         inserirManifestacao()
     elif op == 3:
         print()
     elif op == 4:
-        print()
+        excluirManifestacao()
     elif op == 5:
         print('Finalizando...')
         os.system('cls')

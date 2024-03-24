5bfa9eff91dfc4a99ca998c08f0679f3f9ad61df
joaopaulofg <joao.gomes@ccc.ufcg.edu.br>
2024-03-24 12:36:41 -0300

    adicionado algumas funções auxiliares e primeira funcionalidade do sistema inserirManifestacao

--- Ouvidoria/lib.py
+++ Ouvidoria/lib.py
@@ -16,3 +16,58 @@ def menuPrincipal():
         [4] - Excluir Manifestação
         [5] - Sair 
     ''')
+
+# Leitura de entradas no formato número inteiro.
+def lerInteiro(txt):
+    while True:
+        try:
+            entrada = int(input(txt))
+        except:
+            print("Digite um valor inteiro")
+        else:
+            break
+    return entrada
+
+# Leitura de entradas no formato texto.
+def leiaTexto(txt):
+    while True:
+        try:
+            ent = str(input(txt))
+        except:
+            if ent.isnumeric():
+                print("Formato inválido, tente novamente.")
+            else:
+                break
+        return ent
+
+# Inserção de manifestações no sistema.
+def inserirManifestacao():
+    try:
+        while True:
+            cpfManifestante = leiaTexto("Insira seu CPF: ").strip()
+            break
+        while True:
+            manifestante = leiaTexto("Insira seu nome: ").strip()
+            break
+        while True:
+            manifestacao = leiaTexto("Insira o seu relato da manifestação: ").strip()
+            break
+        while True:
+            telefone_1 = leiaTexto("Insira seu telefone para contato: ").strip()
+            break
+    except:
+        print("Erro na inserçao de dados da manifestação.")
+    else:
+        try:
+            c = con.cursor()
+        except:
+            print("Erro ao conectar no banco de dados;")
+        else:
+            try:
+                sql = ('INSERT INTO ouvidoria(cpfManifestante, manifestante, manifestacao, telefone_1) SELECT "'+cpfManifestante+'", "'+manifestante+'", "'+manifestacao+'", "'+telefone_1+'"')
+                c.execute(sql)
+            except:
+                print("Erro ao inserir manifestação.")
+            else:
+                print("Manifestação inserido com sucesso!")
+                con.commit()
--- Ouvidoria/ouvidoria.py
+++ Ouvidoria/ouvidoria.py
@@ -1,4 +1,4 @@
-from lib import cabecalho, menuPrincipal
+from lib import cabecalho, menuPrincipal, lerInteiro,inserirManifestacao
 import os
 
 cabecalho("ouvidoria do joão")
@@ -6,3 +6,20 @@ cabecalho("ouvidoria do joão")
 while True:
     cabecalho("menu de opções")
     menuPrincipal()
+    op = lerInteiro("ESCOLHA UMA OPÇÃO: ")
+
+    if op == 1:
+        print()
+    elif op == 2:
+        inserirManifestacao()
+    elif op == 3:
+        print()
+    elif op == 4:
+        print()
+    elif op == 5:
+        print('Finalizando...')
+        os.system('cls')
+        print('Programa Encerrado')
+        break
+    else:
+        print("\nOpção inválida.")
